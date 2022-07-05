4ab0c047e4038eb9c303d3688adb935bdb220af5
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2022-07-05 10:52:41 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "maih.h"
+
+/**
+ * print_alphabets - Prints the alphabets in new line followed by \n
+ *
+ * Return : (0) Success
+*/
+
+void print_alphabet(void){
+	char alpha = 'a';
+
+	while (alpha<='z'){
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
