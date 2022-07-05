d01daf5e192db43ac9bc7ec0ee2ba8c67a59f0a9
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2022-07-05 11:28:29 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+*/
+
+void print_alphabet(void){
+	int i;
+	char c;
+	for (i=0; i<=10; i++){
+		c='a';
+		while (c<='z'){
+			_putchar(c);
+			c++
+		}
+		_putchar('\n');
+	}
+}
